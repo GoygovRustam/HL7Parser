7e3d40e5276aa2b043d55f631cc0a8ba4bb3a45d
Rustam Goygov <Rustam.Goygov@medeanalytics.com>
2017-11-20 13:44:28 -0800

    [Implement] Removed different versions of hl7 (Will support only one of the latest versions).  Added definitions for some hl7 segments (v2.6).

--- src/definitionBuilder.ts
+++ src/definitionBuilder.ts
@@ -2,23 +2,10 @@ import { Version } from './models/version';
 import { Hl7Message } from './models/hl7message.model';
 import { Segment } from './models/segment.model';
 
-import { FieldDefinitionV2_1 } from './data/fieldDefinitionV2_1';
-import { FieldDefinitionV2_2 } from './data/fieldDefinitionV2_2';
-import { FieldDefinitionV2_3 } from './data/fieldDefinitionV2_3';
-import { FieldDefinitionV2_3_1 } from './data/fieldDefinitionV2_3_1';
-import { FieldDefinitionV2_4 } from './data/fieldDefinitionV2_4';
-import { FieldDefinitionV2_5 } from './data/fieldDefinitionV2_5';
-import { FieldDefinitionV2_5_1 } from './data/fieldDefinitionV2_5_1';
-import { FieldDefinitionV2_6 } from './data/fieldDefinitionV2_6';
-import { FieldDefinitionV2_7 } from './data/fieldDefinitionV2_7';
+import { FieldDefinitions} from './data/fieldDefinitions';
 
 export class DefinitionBuilder{
-    private _version:Version;
-
-    constructor(version:Version) {
-        this._version = version;
-    }
-
+    /** Adds field definition (Description, length) to every field of the hl7 message*/
     public addDefinitionToHl7Message(hl7Message: Hl7Message){
         this.addDefinition(hl7Message.children)
     }
@@ -27,33 +14,9 @@ export class DefinitionBuilder{
         if(!children) return;
         
         for(var i = 0; i < children.length; i++){
-            children[i].definition = this.getDefinition(children[i].name);
+            let hl7FieldName = children[i].name;
+            children[i].definition = FieldDefinitions.getFieldDefinition(hl7FieldName);
             this.addDefinition(children[i].children);
-        }
-    }
-
-    private getDefinition(hl7FieldName:string){
-        switch(this._version){
-            case Version.v2_1:
-                return FieldDefinitionV2_1.getFieldDefinition(hl7FieldName);
-            case Version.v2_2:
-                return FieldDefinitionV2_2.getFieldDefinition(hl7FieldName);
-            case Version.v2_3:
-                return FieldDefinitionV2_3.getFieldDefinition(hl7FieldName);
-            case Version.v2_3_1:
-                return FieldDefinitionV2_3_1.getFieldDefinition(hl7FieldName);
-            case Version.v2_4:
-                return FieldDefinitionV2_4.getFieldDefinition(hl7FieldName);
-            case Version.v2_5:
-                return FieldDefinitionV2_5.getFieldDefinition(hl7FieldName);
-            case Version.v2_5_1:
-                return FieldDefinitionV2_5_1.getFieldDefinition(hl7FieldName);
-            case Version.v2_6:
-                return FieldDefinitionV2_6.getFieldDefinition(hl7FieldName);
-            case Version.v2_7:
-                return FieldDefinitionV2_7.getFieldDefinition(hl7FieldName);
-            default:
-                throw (`version ${this._version} is not found!`);
         }
     }
 }
